c385c25c42d7b49b1e480014d574fd1de026a422
royels <docmentz@gmail.com>
2018-05-17 23:07:26 -0700

    The swift bird flies over the maple syrup

--- Sources/Currency/Currency.swift
+++ Sources/Currency/Currency.swift
@@ -4,9 +4,9 @@ typealias DictionaryOfGeneric<T: Hashable> = Dictionary<T, Bool>
 class Currency<T> {
     
     
-    static func at(list: [Any], indexes: [Int]) -> [Any] {
+    static func at(list: [T], indexes: [Int]) -> [T] {
         let intSet = Set<Int>(indexes)
-        var returnValue = [Any]()
+        var returnValue = [T]()
         for i in 0..<list.count {
             if(intSet.contains(i)) {
                 returnValue.append(list[i])
@@ -15,17 +15,17 @@ class Currency<T> {
         return returnValue
     }
     
-    static func at(list: [Any], index: Int) -> [Any] {
+    static func at(list: [T], index: Int) -> [T] {
         return at(list: list, indexes: [index])
     }
     
-    static func chunk(list: [Any], size: Int) -> [[Any]] {
+    static func chunk(list: [T], size: Int) -> [[T]] {
         if size <= 0 {
             return [list]
         }
         var counter = 1
-        var returned_list = [[Any]]()
-        var returned_item = [Any]()
+        var returned_list = [[T]]()
+        var returned_item = [T]()
         for item in list {
             returned_item.append(item)
             if counter % size == 0 {
@@ -45,7 +45,7 @@ class Currency<T> {
         return list.filter {$0 != nil } as! [Int]
     }
     
-    static func contains(list: [Int], element: Int) -> Bool {
+    static func contains<T: Hashable>(list: [T], element: T) -> Bool {
         for it in list {
             if (it == element) {
                 return true
@@ -54,31 +54,14 @@ class Currency<T> {
         return false
     }
     
-    static func cycle(list: [Any], repeats: Int, callback: (_ result: Any) -> Any) {
-        for i in 0..<repeats {
+    static func cycle(list: [T], repeats: Int, callback: (_ result: T) -> T) {
+        for _ in 0..<repeats {
             for item in list {
                 _ = callback(item)
             }
         }
     }
     
-    static func difference(list1: [Int], list2: [Int]) -> [Int] {
-        var dict = DictionaryOfGeneric<Int>()
-        if(list1.count > list2.count)
-        {
-            for i in list1 {
-                dict[i] = true
-            }
-            return list2.filter { dict[$0] == true }
-        }
-        else {
-            for i in list2 {
-                dict[i] = true
-            }
-            return list1.filter { dict[$0] == true }
-        }
-    }
-
     static func difference<T: Hashable>(list1: [T], list2: [T]) -> [T] {
         var dict = Set<T>()
             for i in list1 {
@@ -86,6 +69,57 @@ class Currency<T> {
             }
             return list2.filter { dict.contains($0) }
     }
-
+    
+    static func each(list: [T], callback: (_ result: T) -> T) {
+        return cycle(list: list, repeats: 1, callback: callback)
+    }
+    
+    static func every(list: [T], callback: (_ result: T) -> Bool) -> Bool {
+        for i in list  {
+            if( !callback(i)) {
+                return false
+            }
+        }
+        return true
+    }
+    
+    static func factorial(num: T) -> T
+    {
+        var x = 1
+        for i in 1...(num as! Int) {
+            x = x * i
+        }
+        return x as! T
+    }
+    
+    static func fetch<T: Hashable>(list: [T], index: Int, orElse: Int? = nil) -> T
+    {
+        let lookup: Int
+        if(index < list.count) {
+            if(index < 0) {
+                lookup = list.count + index
+                return list[lookup]
+            }
+            return list[index]
+        }
+        return orElse as! T
+    }
+    
+    static func fill<T: Hashable>(_ list: inout [T], _ value: T, _ startIndex: Int? = nil, _ endIndex: Int? = nil)
+    {
+        var begin = 0
+        var end = list.count
+        if let startInd = startIndex {
+            begin = startInd
+        }
+        if let endInd = endIndex {
+            end = endInd
+        }
+        for i in begin..<end {
+            list[i] = value
+        }
+    }
+    
+    
 }
 
--- Tests/CurrencyTests/CurrencyTests.swift
+++ Tests/CurrencyTests/CurrencyTests.swift
@@ -4,19 +4,29 @@ import XCTest
 final class CurrencyTests: XCTestCase {
     
     func testExample() {
-        XCTAssertEqual(Currency.at(list: ["ant", "bat", "cat", "dog", "egg"], indexes: [0, 2, 4]) as! [String], ["ant", "cat", "egg"])
+        XCTAssertEqual(Currency.at(list: ["ant", "bat", "cat", "dog", "egg"], indexes: [0, 2, 4]), ["ant", "cat", "egg"])
     }
     
     func testExample2() {
-        XCTAssertEqual(Currency.at(list: ["ant", "bat", "cat", "dog", "egg"], index: 0) as! [String], ["ant"])
+        XCTAssertEqual(Currency.at(list: ["ant", "bat", "cat", "dog", "egg"], index: 0), ["ant"])
     }
     
     func testExample3() {
-        XCTAssertEqual(Currency.chunk(list: [1,3,5,6,7,3], size: 4) as! [[Int]], [[1,3,5,6],[7,3]])
+        XCTAssertEqual(Currency.chunk(list: [1,3,5,6,7,3], size: 4), [[1,3,5,6],[7,3]])
     }
     
     func testExample4() {
-        XCTAssertEqual(Currency.compact(list: [1,2,3,4,nil,5]), [1,2,3,4,5])
+        XCTAssertEqual(Currency<Any>.compact(list: [1,2,3,4,nil,5] as [Int?]) , [1,2,3,4,5])
     }
-    var currency: Currency = Currency.init()
+    
+    func testExample5() {
+        XCTAssertEqual(Currency.factorial(num: 3), 6 )
+    }
+    
+    func testExample6() {
+        var list = [1,3,4,5,6]
+        Currency<Any>.fill(&list, 3 )
+        XCTAssertEqual(list, [3,3,3,3,3])
+    }
+    
 }
